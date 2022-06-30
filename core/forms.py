0f157ef40cf61430c50f350443963833288b6232
chandu <chandu.vardhan007@gmail.com>
2022-06-29 20:35:51 -0700

    backend django app update

---
+++ core/forms.py
@@ -0,0 +1,8 @@
+from core.models import Profile
+# from django.forms import ModelForm
+from django.forms import ModelForm
+
+class ProfileForm(ModelForm):
+    class Meta:
+        model=Profile
+        exclude=['uuid']
--- core/models.py
+++ core/models.py
@@ -3,22 +3,25 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 import uuid
 
-AGE_CHOICES={
-    ('ALL','ALL'),
+AGE_CHOICES=(
+    ('All','All'),
     ('Kids','Kids')
-}
-MOVIE_CHOICES={
+)
+MOVIE_CHOICES=(
     ('seasonal','Seasonal'),
     ('single','Single')
-}
+)
 
 class CustomUser(AbstractUser):
     profiles=models.ManyToManyField('Profile',blank=True)
 
 class Profile(models.Model):
     name=models.CharField(max_length=225)
-    age_limlit=models.CharField(max_length=10,choices=AGE_CHOICES)
-    uuid=models.UUIDField(default=uuid.uuid4)
+    age_limit=models.CharField(max_length=5,choices=AGE_CHOICES)
+    uuid=models.UUIDField(default=uuid.uuid4,unique=True)
+
+    def __str__(self):
+        return self.name+" "+self.age_limit
 
 class Movie(models.Model):
     title=models.CharField(max_length=225)
